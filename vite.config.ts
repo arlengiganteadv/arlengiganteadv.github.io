e40df5e6ee12c57638c5f7ef8858cc356b8d5976
arlengiganteadv <arlengigante.adv@gmail.com>
2026-05-22 01:52:12 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -153,7 +153,7 @@ function vitePluginManusDebugCollector(): Plugin {
 const plugins = [react(), tailwindcss(), jsxLocPlugin(), vitePluginManusRuntime(), vitePluginManusDebugCollector()];
 
 export default defineConfig({
-  base:'arlengiganteadv.github.io',
+  base: 'arlengiganteadv.github.io',
   plugins,
   resolve: {
     alias: {
